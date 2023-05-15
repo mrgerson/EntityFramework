ca7840f973e20d155483bd0111d0adaadedcf450
Gerson Calvo <gedacaba@gmail.com>
2023-05-15 15:34:56 -0500

    Utilizando base de datos en memoria

--- Program.cs
+++ Program.cs
@@ -11,9 +11,22 @@
 librería para manejar sql server 
  dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 7.0.5
   */
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using proyectoef;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) => 
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+});
 
 app.Run();
